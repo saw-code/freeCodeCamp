ba08a6ee7b0d6d1f6a8425fbaef9b43c49f38d5b
prog_dsp <saw.coding@gmail.com>
2022-09-14 16:36:21 +0300

    Word Blanks

---
+++ blankWords.js
@@ -0,0 +1,10 @@
+// Word Blanks
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "This " + myNoun + " is so " + myAdjective + " and it " + myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
